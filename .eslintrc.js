6ab67adf6cdf41923787d61269f0b2016de835be
feldhaus <maicon.feldhaus@gmail.com>
2020-11-01 19:13:50 +0100

    Add .eslintrc.js file

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ["airbnb-typescript/base"],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "@typescript-eslint/lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
+    "@typescript-eslint/comma-dangle": ["error", "always-multiline"],
+    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+    "no-continue": "off",
+  },
+};
