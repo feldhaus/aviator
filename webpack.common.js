35ed423263379bcb22f158ff2ad24f10676b3e45
feldhaus <maicon.feldhaus@gmail.com>
2020-11-01 18:39:04 +0100

    First project setup

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'The Aviator',
+      inject: true,
+      template: 'src/index.html',
+    }),
+  ],
+};
