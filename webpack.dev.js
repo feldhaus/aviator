040517fbafa7574b5ca9025c254d29e760bab828
feldhaus <maicon.feldhaus@gmail.com>
2020-11-02 17:35:48 +0100

    Config devServer

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,6 +6,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
+    host: '0.0.0.0',
+    port: 8080,
   },
 });
